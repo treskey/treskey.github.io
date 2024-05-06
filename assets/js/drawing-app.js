a5571b50ab61f9be55d891f2f472261e4c7019cd
treskey <tesslockey@icloud.com>
2024-05-06 14:53:53 -0700

    Added doodle tag on hover. added check for mobile -- if so, no functionality.

--- assets/js/drawing-app.js
+++ assets/js/drawing-app.js
@@ -1,84 +1,162 @@
-const canvas = document.getElementById("canvas");
-const clear = document.getElementById("clear-canvas");
-const red = document.getElementById("draw-red");
-const black = document.getElementById("draw-black");
-const yellow = document.getElementById("draw-yellow");
-const ctx = canvas.getContext("2d");
-const toggle = document.getElementsByClassName("logo")[0];
-const toolbar = document.getElementById("toolbar");
+// class Doodle {
+//   constructor(startColor) {
+//     this.canvas = document.getElementById("canvas");
+//     this.ctx = this.canvas.getContext("2d");
+//     this.clearBtn = document.getElementById("clear-canvas");
+//     this.colorWell1 = document.getElementById("draw-red");
+//     this.colorWell2 = document.getElementById("draw-black");
+//     this.colorWell3 = document.getElementById("draw-yellow");
+//     this.activeColor = startColor;
+//     this.ctx.strokeStyle = startColor;
+//     this.isPainting = false;
+//     this.canvas.width = canvas.getBoundingClientRect().width;
+//     this.canvas.height = canvas.getBoundingClientRect().height;
+//     this.canvasOffsetX = canvas.offsetLeft;
+//   }
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-console.log(canvasOffsetX, canvasOffsetY);
+//   draw(e) {
+//     if (!this.isPainting) {
+//       return;
+//     }
 
-canvas.width = canvas.getBoundingClientRect().width;
-canvas.height = canvas.getBoundingClientRect().height;
-console.log(canvas.width, canvas.height);
-let isPainting;
+//     ctx.lineWidth = 2;
+//     ctx.lineCap = "round";
 
-canvas.addEventListener("mousedown", (e) => {
-  isPainting = true;
-  startX = e.clientX;
-  startY = e.clientY;
-});
+//     ctx.lineTo(e.clientX - this.canvasOffsetX, window.scrollY + e.clientY);
+//     ctx.stroke();
+//   }
+// }
 
-canvas.addEventListener("mouseup", (e) => {
-  isPainting = false;
-  ctx.stroke();
-  ctx.beginPath();
-});
+// const doodle = new Doodle("white");
 
-const draw = (e) => {
-  if (!isPainting) {
-    return;
+function isMobile() {
+  var match = window.matchMedia || window.msMatchMedia;
+  if (match) {
+    var mq = match("(pointer:coarse)");
+    return mq.matches;
   }
+  return false;
+}
 
-  ctx.lineWidth = 2;
-  ctx.lineCap = "round";
+if (!isMobile()) {
+  const canvas = document.getElementById("canvas");
+  const clear = document.getElementById("clear-canvas");
+  const red = document.getElementById("draw-red");
+  const black = document.getElementById("draw-black");
+  const yellow = document.getElementById("draw-yellow");
+  const ctx = canvas.getContext("2d");
 
-  ctx.lineTo(e.clientX - canvasOffsetX, window.scrollY + e.clientY);
-  ctx.stroke();
-};
+  // console.log(ctx.strokeStyle);
+  const toggle = document.getElementsByClassName("logo")[0];
+  const toolbar = document.getElementById("toolbar");
+  const doodleMessage = document.getElementById("doodle-message");
+  const canvasOffsetX = canvas.offsetLeft;
+  const canvasOffsetY = canvas.offsetTop;
+  console.log(canvasOffsetX, canvasOffsetY);
 
-function reset() {
   canvas.width = canvas.getBoundingClientRect().width;
   canvas.height = canvas.getBoundingClientRect().height;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log(canvas.width, canvas.height);
+  let isPainting;
+  let currentColor = "white";
+
+  canvas.addEventListener("mousedown", (e) => {
+    isPainting = true;
+    ctx.strokeStyle = currentColor;
+    // startX = e.clientX;
+    // startY = e.clientY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+  });
+
+  canvas.addEventListener("mousemove", draw);
+
+  // canvas.addEventListener("pointerdown", (e) => {
+  //   isPainting = true;
+  //   ctx.strokeStyle = currentColor;
+  //   // startX = e.clientX;
+  //   // startY = e.clientY;
+  // });
+
+  // canvas.addEventListener("pointerup", (e) => {
+  //   isPainting = false;
+  //   ctx.stroke();
+  //   ctx.beginPath();
+  // });
+
+  // canvas.addEventListener("pointermove", draw);
+
+  function draw(e) {
+    if (!isPainting) {
+      return;
+    }
+
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX - canvasOffsetX, window.scrollY + e.clientY);
+    ctx.stroke();
+  }
+
+  function reset() {
+    canvas.width = canvas.getBoundingClientRect().width;
+    canvas.height = canvas.getBoundingClientRect().height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  clear.addEventListener("click", () => {
+    reset();
+  });
+
+  red.addEventListener("click", () => {
+    currentColor = "#ea4646";
+  });
+
+  black.addEventListener("click", () => {
+    currentColor = "white";
+  });
+
+  yellow.addEventListener("click", () => {
+    currentColor = "#efbe43";
+  });
+
+  function toggleIcon() {
+    let icon = document.getElementById("logo-icon");
+    icon.classList = icon.classList.contains("fa-hand-pointer")
+      ? "fa fa-pencil-alt"
+      : "fa fa-hand-pointer";
+    doodleMessage.textContent =
+      doodleMessage.textContent == "doodle! 👀" ? "no doodle 🙅" : "doodle! 👀";
+  }
+
+  toggle.addEventListener("click", () => {
+    canvas.classList.toggle("show");
+    toolbar.classList.toggle("show");
+    ctx.strokeStyle = "white";
+    toggleIcon();
+    reset();
+  });
+
+  toggle.addEventListener("mouseover", (e) => {
+    doodleMessage.style.top = `${e.clientY}px`;
+    doodleMessage.style.left = `${e.clientX + 10}px`;
+    doodleMessage.classList.add("show");
+  });
+
+  toggle.addEventListener("mousemove", (e) => {
+    doodleMessage.style.top = `${e.clientY}px`;
+    doodleMessage.style.left = `${e.clientX + 10}px`;
+  });
+
+  toggle.addEventListener("mouseout", (e) => {
+    doodleMessage.classList.remove("show");
+  });
+  // window.addEventListener("resize", () => {
+  //   canvas.width = canvas.getBoundingClientRect().width;
+  //   canvas.height = canvas.getBoundingClientRect().height;
+  // });
 }
-
-canvas.addEventListener("mousemove", draw);
-
-clear.addEventListener("click", () => {
-  reset();
-});
-
-red.addEventListener("click", () => {
-  ctx.strokeStyle = "#ea4646";
-});
-
-black.addEventListener("click", () => {
-  ctx.strokeStyle = "white";
-});
-
-yellow.addEventListener("click", () => {
-  ctx.strokeStyle = "#efbe43";
-});
-
-function toggleIcon() {
-  let icon = document.getElementById("logo-icon");
-  icon.classList = icon.classList.contains("fa-hand-pointer")
-    ? "fa fa-pencil-alt"
-    : "fa fa-hand-pointer";
-}
-
-toggle.addEventListener("click", () => {
-  canvas.classList.toggle("show");
-  toolbar.classList.toggle("show");
-  ctx.strokeStyle = "white";
-  toggleIcon();
-  reset();
-});
-// window.addEventListener("resize", () => {
-//   canvas.width = canvas.getBoundingClientRect().width;
-//   canvas.height = canvas.getBoundingClientRect().height;
-// });
